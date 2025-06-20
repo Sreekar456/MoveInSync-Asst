22cebbfc83224f3384598d54716f96ffb6ce7193
Sreekar456 <131292958+Sreekar456@users.noreply.github.com>
2025-06-21 00:05:48 +0530

    Rectified the mistake of pushing dummy API key into the git folder

--- user_app/lib/global/global_var.dart
+++ user_app/lib/global/global_var.dart
@@ -1,7 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = '';
-String googleMapKey = 'AIzaSyBA_vNjLrX_xEZG1Lj5h4KKcJZONno8XtE';
+String googleMapKey = '';
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
