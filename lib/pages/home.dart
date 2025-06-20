e95b9ca873b1239b06ab0b94b6f522b4fbae344c
Sreekar <lci2022017@iiitl.ac.in>
2025-06-20 22:31:21 +0530

    Completed first phase of user app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:app/global/global_var.dart'; // Make sure this contains `googlePlexInitialPosition`
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +15,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> googleMapCompleterController =
+      Completer<GoogleMapController>();
+
+  Position? currentPositionOfUser;
+  GoogleMapController? googleMapController;
+
+  Future<void> getCurrentLiveLocation() async {
+    Position positionOfUser = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation,
+    );
+    currentPositionOfUser = positionOfUser;
+
+    LatLng userLatLng = LatLng(
+      currentPositionOfUser!.latitude,
+      currentPositionOfUser!.longitude,
+    );
+
+    CameraPosition cameraPosition = CameraPosition(
+      target: userLatLng,
+      zoom: 15,
+    );
+
+    googleMapController?.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: TextStyle(fontSize: 20,color:Colors.white),
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: googlePlexInitialPosition,
+            onMapCreated: (GoogleMapController controller) {
+              googleMapController = controller;
+              updateMapTheme(controller);
+              if (!googleMapCompleterController.isCompleted) {
+                googleMapCompleterController.complete(controller);
+              }
+              getCurrentLiveLocation();
+            },
+          ),
+        ],
       ),
     );
+  }
+
+  /// Loads and applies the custom map theme
+  void updateMapTheme(GoogleMapController controller) {
+    getJsonFileFromThemes("themes/dark_style.json").then(
+      (style) => setGoogleMapStyle(style, controller),
+    );
+  }
+
+  /// Loads the map style JSON file from the assets
+  Future<String> getJsonFileFromThemes(String mapStylePath) async {
+    ByteData byteData = await rootBundle.load(mapStylePath);
+    final list = byteData.buffer.asUint8List(
+      byteData.offsetInBytes,
+      byteData.lengthInBytes,
+    );
+    return utf8.decode(list);
+  }
+
+  /// Applies the map style to the GoogleMapController
+  void setGoogleMapStyle(String googleMapStyle, GoogleMapController controller) {
+    controller.setMapStyle(googleMapStyle);
   }
 }
