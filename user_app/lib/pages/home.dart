564f824989cc03564804db99ffb9c8861ef694ac
Sreekar <lci2022017@iiitl.ac.in>
2025-06-21 12:53:07 +0530

    Added options of logout and profile on the home page displayed over the map

--- user_app/lib/pages/home.dart
+++ user_app/lib/pages/home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:permission_handler/permission_handler.dart';
 import 'package:app/global/global_var.dart'; // Make sure this contains `googlePlexInitialPosition`
 
 class HomePage extends StatefulWidget {
@@ -17,6 +17,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final Completer<GoogleMapController> googleMapCompleterController =
       Completer<GoogleMapController>();
+
+  final GlobalKey<ScaffoldState> sKey = GlobalKey<ScaffoldState>();
 
   Position? currentPositionOfUser;
   GoogleMapController? googleMapController;
@@ -45,6 +47,82 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: sKey,
+      drawer: Container(
+        width: 255,
+        color: Colors.black87,
+        child: Drawer(
+          backgroundColor: Colors.white10,
+          child: ListView(
+            children: [
+              Container(
+                color: Colors.black87,
+                height: 160,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.black87,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/avatarman.png",
+                        height: 60,
+                        width: 60,
+                      ),
+                      const SizedBox(width: 16),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(userName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                          const SizedBox(height: 4),
+                          Text("Profile",
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),)
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )  ,
+              // header
+              // body
+              const Divider(height: 1,color: Colors.white,thickness: 1,),
+
+              const SizedBox(height: 10,),
+              ListTile(
+                leading: IconButton(onPressed: (){}, icon: const Icon(Icons.info, color: Colors.grey,)),
+                title: const Text(
+                  "About",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                
+              ),
+              ListTile(
+                leading: IconButton(onPressed: (){}, icon: const Icon(Icons.logout, color: Colors.grey,)),
+                title: const Text(
+                  "Logout",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
           GoogleMap(
@@ -60,6 +138,40 @@ class _HomePageState extends State<HomePage> {
               getCurrentLiveLocation();
             },
           ),
+
+
+
+          Positioned(
+            top: 36,
+            left: 10,
+            child: GestureDetector(
+              onTap: (){
+
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7,0.7),
+                    ),
+                  ],
+                ),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
